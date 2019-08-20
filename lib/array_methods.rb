8d465945ce6bd9aa284c9d0e689fb46acbf71650
Eddie Parker <eddie.parker88@gmail.com>
2019-08-20 21:00:25 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -2,7 +2,10 @@ def find_element_index(array, value_to_find)
   # Add your solution here
   scale = [1,2,3,4,5]
  
-scale.length.times { |index|
+scale.length.times { |index| 4
+  if then
+    
+end
   puts array[index]
 }
 end
