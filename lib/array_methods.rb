3680c45842a41f3faba32cfa0c1465a21d798200
Eddie Parker <eddie.parker88@gmail.com>
2019-08-20 22:19:07 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -4,7 +4,8 @@ def find_element_index(array, value_to_find)
    if array[count] == value_to_find
      return count
    end
-   nil
+   if array[count] != value_to_find
+     return nil
  end
 end
 
